bfb635b3819903ed400b7452b032b6160a82d10e
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-04 08:13:00 +0300

    pass data using useContext

--- src/context/User.js
+++ src/context/User.js
@@ -1,12 +1,17 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 // Context obj
 const UserContext = createContext();
 
 // Context Provider Component
 function UserProvider({children}){
+    // const currentUser = {
+    //     name: "Charles",
+    //     interests : ["coding", "cycling", "mma"]
+    // }
+    const [user, setUser] = useState(null);
     return (
-        <UserContext.Provider value={null}>
+        <UserContext.Provider value={{user, setUser}}>
             {children}
         </UserContext.Provider>
     )
